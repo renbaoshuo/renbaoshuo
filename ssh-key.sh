945267e3fd8543306f0cf1acc2fd4eeeab85a999
Ren Baoshuo <i@baoshuo.ren>
2021-04-11 08:56:45 +0800

    Add ssh public key

---
+++ ssh-key.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p ~/.ssh
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCVy8OJ+Yh0NTNCswHkkqFPnYoO4EDLZIr5Uf49ktYlscbwsEVv0wHJ5nQnbwijcVdtVr1aMoCqAQQiTnkCN5JlTKY0NTELvX/Re8yIPpHt0LXnZ4DRNJKjyasKGoMqtk5eJprrpqUshKXnIajAeR1uURT70SPnSHgDVmBO2C8n8dP5DUZaYIIJf2EzxerZJagrVvjB1WcWlmNn8waO5E6VPbiIk7MlPcqE9ICHtGBv7CJpEMNmOofP/d+pbZ7Sghe2BiMQ6BSFMGNill3ctlqL/Qde/TDlqzr75H/JwaeEgGrJGYtrthdtSH75rxoN0oNsPSO6aTAZd6GozTNOaArQHVkNjYlE8qfEPHZqJAtV3wTGJO+fa4T1E0zn36KHSptJYxrZ8VYNS+/YRd4lnAcvrlfhmO6DVq7YZkZvaLMUM2kF7EEjmO+yV++M6nzyeJ38eSVxv2fuBvqdyQ1oD8UhzmkgSiNVnuXqvHisI5MxEXFOfz6ml8WlIKZ+B1JVF9M= renbaoshuo" >> ~/.ssh/authorized_keys
+echo "[INFO] Successfully added SSH key: renbaoshuo"
