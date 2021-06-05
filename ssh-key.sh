d531708caef88a84c93de76fde49c5240e92f221
Ren Baoshuo <i@baoshuo.ren>
2021-06-05 22:38:04 +0800

    Add: @BaoshuoBot's ssh key

--- ssh-key.sh
+++ ssh-key.sh
@@ -3,6 +3,8 @@
 mkdir -p ~/.ssh
 curl -Ls https://github.com/renbaoshuo.keys >> ~/.ssh/authorized_keys
 echo -e "\033[32mINFO\033[0m Successfully added SSH key: renbaoshuo"
+curl -Ls https://github.com/BaoshuoBot.keys >> ~/.ssh/authorized_keys
+echo -e "\033[32mINFO\033[0m Successfully added SSH key: BaoshuoBot"
 sed -i 's/^#\?PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
 PasswordAuthentication=$(cat /etc/ssh/sshd_config | grep "PasswordAuthentication no" | awk '{print $2}')
 echo -e "\033[32mINFO\033[0m Password authentication: ${PasswordAuthentication}"
